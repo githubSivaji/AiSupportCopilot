21650a5f9d272edec9d5a01335affafe34a80b5a
githubSivaji <sivajid0@gmail.com>
2026-08-27 00:28:08 +0530

    inventory setup completed

--- ai-support-copilot/src/main/java/com/sivaji/aisupportcopilot/entity/Inventory.java
+++ ai-support-copilot/src/main/java/com/sivaji/aisupportcopilot/entity/Inventory.java
@@ -1,4 +1,28 @@
 package com.sivaji.aisupportcopilot.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Inventory {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private UUID productId;
+
+    @Column(nullable = false)
+    private Integer availableQuantity;
+
+    @Version
+    private Long version;
 }
